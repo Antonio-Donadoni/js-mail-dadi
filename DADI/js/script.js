6fc9face1fbfe7991b1c095e94d819b80eb97552
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-06-23 21:22:15 +0200

    ADD ARRAY + FOR per icone dadi // ADD CSS Generale

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -9,43 +9,24 @@ var dadoUser = Math.floor(Math.random()*6) +1;
 var dadoAI = Math.floor(Math.random()*6) +1;
 console.log(dadoUser);
 console.log(dadoAI);
-document.getElementById('risultato-user').innerHTML = "Il tuo punteggio: ";
-document.getElementById('risultato-ai').innerHTML = "Il punteggio dell'avversario: ";
+document.getElementById('risultato-user').innerHTML = "IL TUO PUNTEGGIO ";
+document.getElementById('risultato-ai').innerHTML = "PUNTEGGIO AVVERSARIO";
 
-if ( dadoUser == 1) {
-  document.getElementById("dado-user").className = "fas fa-dice-one";
+var facceDadi = ["", "fas fa-dice-one", "fas fa-dice-two", "fas fa-dice-three", "fas fa-dice-four", "fas fa-dice-five", "fas fa-dice-six"  ]
+
+for (var i = 0; i < facceDadi.length; i++) {
+if ( i == dadoUser) {
+    document.getElementById("dado-user").className = facceDadi[i];
+    break;
 }
-  else if (dadoUser == 2) {
-    document.getElementById("dado-user").className = "fas fa-dice-two";
-   }
-  else if (dadoUser == 3) {
-    document.getElementById("dado-user").className = "fas fa-dice-three";
-   }
-  else if (dadoUser == 4) {
-    document.getElementById("dado-user").className = "fas fa-dice-four";
-    }
-  else if (dadoUser == 5) {
-    document.getElementById("dado-user").className = "fas fa-dice-five";
-   }
-  else   document.getElementById("dado-user").className = "fas fa-dice-six";
+}
 
-
-  if ( dadoAI == 1) {
-    document.getElementById('dado-ai').className = "fas fa-dice-one";
-  }
-    else if (dadoAI == 2) {
-      document.getElementById('dado-ai').className = "fas fa-dice-two";
-     }
-    else if (dadoAI == 3) {
-      document.getElementById('dado-ai').className = "fas fa-dice-three";
-     }
-    else if (dadoAI == 4) {
-      document.getElementById('dado-ai').className = "fas fa-dice-four";
-      }
-    else if (dadoAI == 5) {
-      document.getElementById('dado-ai').className = "fas fa-dice-five";
-     }
-    else   document.getElementById('dado-ai').className = "fas fa-dice-six";
+for (var i = 0; i < facceDadi.length; i++) {
+if ( i == dadoAI) {
+    document.getElementById("dado-ai").className = facceDadi[i];
+    break;
+}
+}
 
 
 if (dadoUser > dadoAI) {
